572d7e52fae0a7844ba7a04f1a9a251b5cc29771
ryudh1012 <ryudh1012@gmail.com>
2024-03-21 19:18:02 +0900

    source

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -6,9 +6,13 @@ public class Triangle {
             }
             System.out.println("");
         }
-        for (int i = 1; i < 4; i++) {
-            for (int j = 4; j > i; j--) {
-                System.out.print("*");
+        for(int i=3; i>0; i--){
+            for(int j=3; j>0; j--){
+                if(i>=j){
+                    System.out.print("*");
+                }else{
+                    System.out.print("");
+                }
             }
             System.out.println("");
         }
