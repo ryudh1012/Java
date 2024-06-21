ce310352ab69c95ea82a4dde3d6f3b05491472aa
ryudh1012 <ryudh1012@gmail.com>
2024-06-21 20:22:29 +0900

    source

---
+++ src/main/java/Ex13_11.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ex13_11 {
+    static class MyGUI extends JFrame{
+        MyGUI() {
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setTitle("이벤트 처리 1");
+
+            this.setLayout(new FlowLayout());
+
+            JButton btn1 = new JButton("버튼1");
+            btn1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    btn1.setBackground(Color.RED);
+
+                }
+            });
+            this.add(btn1);
+
+            setSize(200, 200);
+            setVisible(true);
+        }
+    }
+
+    public static void main(String[] args){
+        new MyGUI();
+    }
+}
